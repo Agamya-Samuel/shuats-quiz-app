5a1ce4583f8a19ff382058556d6a11149f1533da
Agamya Samuel <github@agamya.dev>
2025-01-29 11:12:07 +0530

    Enhance Question schema validation to ensure unique option texts are case-insensitive

--- db/models/question.ts
+++ db/models/question.ts
@@ -15,11 +15,15 @@ const QuestionSchema: Schema<IQuestion> = new Schema({
 
 // Custom validation to ensure unique option texts
 QuestionSchema.pre('save', function (next) {
-	const optionTexts = this.options.map(option => option.text);
+	const optionTexts = this.options.map((option) =>
+		option.text.trim().toLowerCase()
+	);
 	const uniqueOptionTexts = new Set(optionTexts);
 
 	if (uniqueOptionTexts.size !== optionTexts.length) {
-		return next(new Error('Option texts must be unique for each question.'));
+		return next(
+			new Error('Option texts must be unique for each question.')
+		);
 	}
 
 	next();
