d289e45363e3277c4e3eac97fa31f5ed9af7e7e5
Agamya Samuel <github@agamya.dev>
2025-02-23 20:42:52 +0530

    Add maintainer login form and page for authentication

---
+++ app/admin/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+// app/(auth)/login/page.tsx
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { LoginForm } from './forms/LoginForm';
+
+export default function LoginPage() {
+	const searchParams = useSearchParams();
+	const redirect = searchParams.get('redirect');
+
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<div className="w-full max-w-md">
+				{/* <h1 className="text-2xl font-bold mb-4">Login</h1> */}
+				<LoginForm redirect={redirect || undefined} />
+			</div>
+		</div>
+	);
+}
